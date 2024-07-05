494d22cd3289a7256552fdbd7e33c5005c1ca5fa
natthanan-bit <6511130036@mut.ac.th>
2024-07-05 17:00:31 +0700

    commit5

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
     echo "<h1>XD</h1>"
     echo "<h1>XDD</h1>"
+    echo "<h1>XDDD</h1>"
     ?>
--- menu.php
+++ menu.php
@@ -3,4 +3,5 @@
     echo "<h1>XD</h1>"
     echo "<h1>XDD</h1>"
     echo "<h1>XDDD</h1>"
+    echo "<h1>XDDDD</h1>"
 ?>
